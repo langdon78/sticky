99f59a02ca2b5a64ce77b83040f76d181a5a47bf
James Langdon <jlangdon@switchmatehome.com>
2017-12-31 15:56:51 +0100

    Initial commit

---
+++ sticky/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sticky
+//
+//  Created by James Langdon on 12/31/17.
+//  Copyright © 2017 James Langdon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
