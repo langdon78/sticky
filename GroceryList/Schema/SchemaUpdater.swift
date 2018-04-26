45fafe7f3f99bd127564ceb96c85a2087a1c0be3
James Langdon <corporatelangdon@gmail.com>
2018-04-26 17:10:39 -0400

    Remove process control methods

--- GroceryList/Schema/SchemaUpdater.swift
+++ GroceryList/Schema/SchemaUpdater.swift
@@ -1,37 +1,12 @@
 import Foundation
 import Sticky
 
-class StoredDataSchemaUpdater {
-    fileprivate static let schemaFileExtension = "json"
+struct StoredDataSchemaUpdater: StickySchemaUpdateable {
+    static var schemaFileExtension = "json"
     // Set [{version}: {fileName}]
-    fileprivate static let fileVersionMap: [Int: String] = [
-        1: "sticky_schema_1"
-//        2: "sticky_schema_2",
-//        3: "sticky_schema_3"
+    static var fileVersionMap: [Int: String] = [
+        1: "sticky_schema_1",
+        2: "sticky_schema_2",
+        3: "sticky_schema_3"
     ]
-    
-    fileprivate static var schemaFiles: [StickySchemaFile] {
-        let bundle = Bundle.main
-        return fileVersionMap
-            .compactMap { fileVersion in
-                guard let url = bundle.url(
-                    forResource: fileVersion.value,
-                    withExtension: schemaFileExtension
-                    ) else { return nil }
-                return StickySchemaFile(version: fileVersion.key, fileUrl: url)
-        }
-            .filter { $0.version > Sticky.shared.currentSchemaVersion }
-    }
-    
-    fileprivate static var maxVersion: Int {
-        return fileVersionMap
-            .sorted { $0.key < $1.key }
-            .last?.key ?? 0
-    }
-    
-    static func processUpdates() {
-        if StickySchemaUpdater.checkUpdateNeeded(for: maxVersion) {
-            StickySchemaUpdater.processUpdates(for: schemaFiles)
-        }
-    }
 }
