2529cc25727739c8c04c1444d06cf27b83f68788
James Langdon <jlangdon@switchmatehome.com>
2018-06-15 07:59:08 -0400

    Replace default directory
    The "Documents" directory is meant for user created content while
    the "Library/Application Support" directory is meant for app
    generated data. Both are backed up by iCloud. This is defined in
    Apple's documentation. https://developer.apple.com/library/archive/documentation/FileManagement/Conceptual/FileSystemProgrammingGuide/FileSystemOverview/FileSystemOverview.html

--- sticky/Configuration/StickyConfiguration.swift
+++ sticky/Configuration/StickyConfiguration.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 public let defaultFileExtension = ".json"
+fileprivate let defaultBundleId = "com.sticky"
 
 public struct StickyConfiguration {
     public let localDirectory: URL
@@ -10,15 +11,34 @@ public struct StickyConfiguration {
     public let async: Bool
     public let logStyle: StickyLogStyle
     public let rollbackToSchemaVersion: Int?
-    
-    public init(
-        localDirectory: URL = try!
-            FileManager.default.url(
-                for: .documentDirectory,
+    public static var defaultDirectory: URL? {
+        do {
+            // Using /Library/Application Support/ for app data files
+            // backed up by iCloud
+            // https://developer.apple.com/library/archive/documentation/FileManagement/Conceptual/FileSystemProgrammingGuide/FileSystemOverview/FileSystemOverview.html
+            var appSupport = try FileManager.default.url(
+                for: .applicationSupportDirectory,
                 in: .allDomainsMask,
                 appropriateFor: nil,
-                create: false
-            ),
+                create: true
+            )
+            // Create subdirectory for bundleId
+            appSupport = appSupport.appendingPathComponent(Bundle.main.bundleIdentifier ?? defaultBundleId, isDirectory: true)
+            if !FileManager.default.fileExists(atPath: appSupport.path) {
+                try FileManager.default.createDirectory(at: appSupport,
+                                     withIntermediateDirectories: false)
+            }
+            
+            return appSupport
+        }
+        catch {
+            stickyLog(error)
+        }
+        return nil
+    }
+    
+    public init(
+        localDirectory: URL = defaultDirectory!,
         preloadCache: Bool = true,
         fileExtensionName: String = defaultFileExtension,
         clearDirectory: Bool = false,
